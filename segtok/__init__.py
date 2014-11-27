d9df9d536ae872f07dfb5a83d08281a642645ded
Florian Leitner <florian.leitner@gmail.com>
2014-11-27 17:03:12 +0100

    fixed license statement

--- segtok/__init__.py
+++ segtok/__init__.py
@@ -3,5 +3,5 @@
    :synopsis: a sentence segmentation and word tokenization library
 
 .. moduleauthor:: Florian Leitner <florian.leitner@gmail.com>
-.. License: GNU Affero GPL v3 (http://www.gnu.org/licenses/agpl.html)
+.. License: MIT <http://opensource.org/licenses/MIT>
 """
