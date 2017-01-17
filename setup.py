63863e7a96d36ae46f70113fedcf0c2ca066a710
fnl <me@fnl.es>
2017-01-17 10:43:21 +0100

    Version bump (to 1.5.2).

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name='segtok',
-    version='1.5.1',
+    version='1.5.2',
     url='https://github.com/fnl/segtok',
     author='Florian Leitner',
     author_email='florian.leitner@gmail.com',
